7dd778f600cdf0e025d65c41020a24b61c3ee4ed
Rainer Willinger <rainer.willinger@aon.at>
2012-04-11 23:03:09 +0200

    added empty class for Wuerfel and tried java script on click for Wuerfel

---
+++ src/java/model/Wuerfel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author rainer
+ */
+public class Wuerfel {
+    private String image = "";
+    
+    public Wuerfel() {
+        
+    }
+}
