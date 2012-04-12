863321c9e605e99012e94f8c2969f16eb5031441
Rainer Willinger <rainer.willinger@aon.at>
2012-04-12 08:48:24 +0200

    random number for Wuerfel und GameServlet

--- src/java/model/Wuerfel.java
+++ src/java/model/Wuerfel.java
@@ -4,14 +4,24 @@
  */
 package model;
 
+import java.util.Random;
+
 /**
  *
  * @author rainer
  */
 public class Wuerfel {
     private String image = "";
+    private int rndNumber = 0;
+    private Random generator = null;
     
     public Wuerfel() {
+        this.generator = new Random();
+    }
+    
+    public int wuerfeln() {
+        rndNumber = generator.nextInt(6) + 1;
         
+        return rndNumber;
     }
 }
